bf6793ec82ebbedf73158dc97ec21418fe0b4bc4
comeFromFarAway <76560595+comeFromFarAway@users.noreply.github.com>
2022-11-23 17:07:16 +0000

    Deploying to gh-pages from  @ 82f54925f80464d81a9ee6d0b6a2910e4d8ab2cd 🚀

---
+++ precache-manifest.8cbb3845239d4c23991f0dd5cb6a4302.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e7fe0250ac360832ad6d947fe7cb32",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "d470bc933d1cded9086c",
+    "url": "/github-actions-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "91478de87f55e553801c",
+    "url": "/github-actions-demo/static/js/2.1b414692.chunk.js"
+  },
+  {
+    "revision": "d470bc933d1cded9086c",
+    "url": "/github-actions-demo/static/js/main.cb633046.chunk.js"
+  },
+  {
+    "revision": "0fb876f3726ebbfc77a4",
+    "url": "/github-actions-demo/static/js/runtime~main.b2b0a7b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github-actions-demo/precache-manifest.459014fcec9a515d893882de5605d51f.js"
+  "/github-actions-demo/precache-manifest.8cbb3845239d4c23991f0dd5cb6a4302.js"
 );
 
 self.addEventListener('message', (event) => {
